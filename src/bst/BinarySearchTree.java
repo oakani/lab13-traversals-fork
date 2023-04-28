29e0d39268dcc23343574c7e528e22d649d9786a
Joseph Hawkins <hawkinsjoseph2003@gmail.com>
2023-04-28 08:34:44 -0700

    please work

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -139,7 +139,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//then recursively print the right side of current node
 	//For a bst this will print the values in sorted order from smallest to largest
 	public void inOrder() {
-		System.out.println("inorder");
+		System.out.println("inorder mines");
 		inOrderRecurse(root); 
 	}
 	
